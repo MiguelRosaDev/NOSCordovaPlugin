058bf70c43c13f905835d6f580bf7fa048e2a690
miguelrosanos <79104255+miguelrosanos@users.noreply.github.com>
2021-05-20 12:52:56 +0100

    Update NOSPluginExternalWebView.swift

--- src/ios/NOSPluginExternalWebView.swift
+++ src/ios/NOSPluginExternalWebView.swift
@@ -91,7 +91,14 @@ class NOSPluginExternalWebView: UIViewController, WKNavigationDelegate {
                         request.setValue(headerValue, forHTTPHeaderField: headerName)
                         
                         if(headerName.contains("UserAgent")){
-                            let originalUserAgent = webView?.evaluateJavaScript("navigator.userAgent", completionHandler: nil) + headerValue
+                            //let originalUserAgent = UIWebView().stringByEvaluatingJavaScript(from: "navigator.userAgent")! + headerValue
+                            //let originalUserAgent = webView?.evaluateJavaScript("navigator.userAgent", completionHandler: nil) + headerValue        
+                            
+                            webView?.evaluateJavaScript("navigator.userAgent", completionHandler: nil) { (result, error) in
+                                if error == nil {
+                                    let originalUserAgent = result + headerValue
+                                }
+                            }
                             
                             defaults.register(defaults: ["UserAgent": originalUserAgent])
                             webView?.customUserAgent = headerValue
